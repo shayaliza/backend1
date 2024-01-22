e7d9e67ac12b3b9aaf369f05627f089811dff18c
shayaliza <magicalmind01@gmail.comgit statuss>
2024-01-22 22:44:44 +0530

    a

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const userRoutes = require("./Routes/userRouter");
 const gotanRoutes = require("./Routes/gotanRouter");
 //@CORS setup///////////////////
 const corsOptions = {
-  origin: ["https://digistall.in", "http://localhost:3000", "https://gotan.in"],
+  origin: [
+    "https://digistall.in",
+    "https://admin.digistall.in",
+    "http://localhost:3000",
+    "https://gotan.in",
+    // "http://ajmer.localhost:3000",
+  ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
@@ -27,6 +33,7 @@ app.use(bodyParser.json());
 //@MongoSetup///////////////////////////////////////////////////////
 const mongoDBURL =
   "mongodb+srv://chai:chaiforlife@cluster0.hn1kv1u.mongodb.net/?retryWrites=true&w=majority";
+// "mongodb+srv://hriDev:plcR3WNQn6iMETa2@hri.gb755zn.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(mongoDBURL, {
   // useNewUrlParser: true,
@@ -53,6 +60,78 @@ process.on("SIGINT", () => {
     process.exit(0);
   });
 });
+//@test//////////////////////////////////////////
+// Create a customer collection and define a schema
+// const customerSchema = new mongoose.Schema({
+//   name: String,
+//   phoneNumber: String,
+//   email: String,
+//   orders: {
+//     type: [
+//       {
+//         orderId: String,
+//         productId: String,
+//         gst: Number,
+//         productImage: String,
+//         productName: String,
+//         hasVariants: Boolean,
+//         size: String,
+//         onePiecePrice: Number,
+//         orderNote: {
+//           deliveryPartner: String,
+//           ShippingId: String,
+//           deliveryDate: String,
+//           deliveryTime: String,
+//         },
+//         color: String,
+//         shopId: String,
+//         paymentMethod: String,
+//         productCount: Number,
+//         totalCartValue: Number,
+//         orderStatus: { type: String, default: "PENDING" },
+//         address: String,
+//         date: { type: Date, default: Date.now },
+//       },
+//     ],
+//     default: [],
+//   },
+//   address: {
+//     type: Object,
+//     default: {},
+//   },
+// });
+
+// const Customer = mongoose.model("Customer", customerSchema);
+
+// // API endpoint to add a new customer
+// app.post("/api/customers", async (req, res) => {
+//   try {
+//     const { name, phoneNumber, email } = req.body;
+//     const newCustomer = new Customer({ name, phoneNumber, email });
+//     const savedCustomer = await newCustomer.save();
+//     res.status(201).json(savedCustomer);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+// app.post("/api/customers/byPhoneNumber", async (req, res) => {
+//   try {
+//     const { phoneNumber } = req.body;
+//     const customer = await Customer.findOne({ phoneNumber });
+
+//     if (customer) {
+//       res.status(200).json(customer);
+//     } else {
+//       res.status(404).json({ error: "Customer not found" });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+//@test//////////////////////////////////////////
 //@Root api////////////////////////////////////////////////////////////
 app.get("/", (req, res) => {
   res.send("Welcome to the home route!");
